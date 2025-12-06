483e7ef28664439865d54a984b63bb087d4516b1
Jonathan Phillips <jvp@Mac.localdomain>
2025-12-05 22:20:08 -0600

    2024 5-2 solution

--- 2024/05/05-2.pl
+++ 2024/05/05-2.pl
@@ -16,48 +16,37 @@ while (<>) {
     if ($on_sets) {
         push @sets, [ split(/,/, $_) ];
     } else {
-        push @rules, [ split(/\|/, $_) ];
+        push @rules, $_;
     }
 }
 
 my $total = 0;
-foreach my $set (@sets) {
-    print "set: " . join(",", @{$set}) . "\n";
-
-    my @relevant_rules = ( );
-    foreach my $rule (@rules) {
-        my $first = @{$rule}[0];
-        my $second = @{$rule}[1];
-        if (grep(/^$first$/, @{$set}) && grep(/^$second$/, @{$set})) {
-            print " - Relevant: $first,$second\n";
-        }
-    }
-    next;
-
-    my $good = 1;
-    foreach my $rule (@rules) {
-        #print "  - rule: " . join(",", @{$rule}) . "\n";
-        last unless $good;
-        my $first = @{$rule}[0];
-        my $second = @{$rule}[1];
-        my $found_first = undef;
-        my $found_second = undef;
-        foreach my $counter (0..$#{$set}) {
-            if (@{$set}[$counter] == $first) {
-                $found_first = $counter;
-            }
-            if (@{$set}[$counter] == $second) {
-                $found_second = $counter;
+foreach my $s (@sets) {
+    my @set = @{$s};
+    my $flipped_any_this_pass = 0;
+    my $this_set_was_out_of_order = 0;
+    # walk through the set, flipping any out of order page groupings.
+    # Do this repeatedly until there is nothing to flip
+    do {
+        $flipped_any_this_pass = 0;
+        foreach my $index (0.. $#set - 1) {
+            my $first = $set[$index];
+            my $second = $set[$index+1];
+            if (grep(/^$second\|$first$/, @rules)) {
+                # these pages are out of order. Flip them.
+                my $temp = $set[$index+1];
+                $set[$index+1] = $set[$index];
+                $set[$index] = $temp;
+                $flipped_any_this_pass = 1;
+                $this_set_was_out_of_order = 1;
             }
         }
-        if (defined $found_first && defined $found_second) {
-            $good = 0 unless $found_second > $found_first;
-        }
-    }
-    if ($good) {
-        print "Good set: " . join(",", @{$set}) . "\n";
-        my $middle = (scalar @{$set} + 1) / 2;
-        $total += @{$set}[$middle-1];
+    } until $flipped_any_this_pass == 0;
+
+    if ($this_set_was_out_of_order) {
+        my $middle = (scalar @set + 1) / 2;
+        $total += $set[$middle-1];
     }
 }
+
 print "Total = $total\n";
